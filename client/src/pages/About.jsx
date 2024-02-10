1f854774473d93700dabc9e6eb5412fcdacc779a
Ville Suokas <vilsuo1@gmail.com>
2024-02-10 18:30:48 +0200

    updated about page to use health check api

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,17 +1,38 @@
 import axios from "axios";
+import { useState } from "react";
 
 const About = () => {
+  const [status, setStatus] = useState();
 
   const handleClick = async () => {
-    const { data } = await axios.get('/api/ping');
-    console.log(data);
+    try {
+      const response = await axios.get('/api/health');
+      setStatus({
+        type: 'success',
+        message: response.data.message,
+        time: new Date(),
+      });
+    } catch (error) {
+      setStatus({
+        type: 'error',
+        message: error.message,
+        time: new Date(),
+      });
+    }
   };
 
   return (
     <div className="about-page">
       <h2>About</h2>
 
-      <button onClick={handleClick}>Ping</button>
+      <button onClick={handleClick}>Check status</button>
+
+      <div className="status">
+        { status
+          ? <p>{status.time.toString()}: {status.message}</p>
+          : <p>Unknown status</p>
+        }
+      </div>
     </div>
   );
 };
