c3bc1db14b70130c0dd0f16f2e92a5d4e5832daf
Ville Suokas <vilsuo1@gmail.com>
2024-02-12 14:20:10 +0200

    tested note page back navigation

--- client/src/pages/Note.test.js
+++ client/src/pages/Note.test.js
@@ -1,20 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
-
-import Note from './Note';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
+import Note from './Note';
+import Notes from './Notes';
+
 const note = {
-  id: 3,
-  content: 'testing',
-  views:3,
+  id: 1,
+  content: 'The first note!',
+  views: 1,
   createdAt: '2024-02-11T23:14:44.770Z',
 };
 
-const notePath = `/notes/${note.id}`;
+const notesRoute = {
+  path: '/notes',
+  element: <Notes />,
+  loader: () => [note],
+};
 
 const noteRoute = {
-  path: notePath,
+  path: `/notes/${note.id}`,
   element: <Note />,
   loader: () => note,
 };
@@ -24,7 +30,7 @@ describe('<Note />', () => {
     const routes = [noteRoute];
 
     const router = createMemoryRouter(routes, {
-      initialEntries: [notePath],
+      initialEntries: [noteRoute.path],
     });
 
     await act(async () => {
@@ -35,9 +41,25 @@ describe('<Note />', () => {
     expect(element).toBeDefined();
   });
 
-  /*
   test('can navigate to the Notes page', async () => {
+    const routes = [notesRoute, noteRoute];
 
+    const router = createMemoryRouter(routes, {
+      initialEntries: [notesRoute.path, noteRoute.path],
+      initialIndex: 1,
+    });
+
+    await act(async () => {
+      render(<RouterProvider router={router} />);
+    });
+
+    // navigate to the notes page
+    const user = userEvent.setup();
+    const notesLink = screen.getByRole('link', { name: /Back/i });
+    await user.click(notesLink);
+
+    // expect to find the main heading
+    expect(screen.getByRole('heading', { name: /^Notes$/i }))
+      .toBeInTheDocument();
   });
-  */
 });
