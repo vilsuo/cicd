6fb6a2c6aaae5fd8424312e8a39eefc33f52c3b2
Ville Suokas <vilsuo1@gmail.com>
2024-02-15 18:52:43 +0200

    postgres database runs in containers in test and development environments. Test setup syncs and drops the tables

--- server/src/config.js
+++ server/src/config.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 
+// `${DB_DIALECT}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+const POSTGRES_URL_TEST = 'postgres://postgres:secret@localhost:5434/cicd_test';
+const POSTGRES_URL_DEV = 'postgres://postgres:secret@localhost:5433/cicd_dev';
+
+const POSTGRES_URLS = {
+  test: POSTGRES_URL_TEST,
+  development: POSTGRES_URL_DEV,
+  production: process.env.POSTGRES_URL,
+};
+
 module.exports = {
-  POSTGRES_URL: (process.env.NODE_ENV !== 'test')
-    ? process.env.POSTGRES_URL
-    : process.env.POSTGRES_URL_TEST,
+  POSTGRES_URL: POSTGRES_URLS[process.env.NODE_ENV],
   PORT: process.env.PORT || 3000,
 };
