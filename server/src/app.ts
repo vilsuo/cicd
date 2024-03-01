3b0e344c9e85b32466a3f0bcb544cd661a21c4f5
vilsuo <vilsuo1@gmail.com>
2024-03-01 02:19:47 +0200

    changed static dist folder path in app

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use('/api', router);
 
-const DIST = path.join(__dirname, '..', 'dist');
+const DIST = path.join(__dirname, '..', 'build', 'dist');
 
 app.use(express.static(DIST));
 
